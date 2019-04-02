f4a70ee646238bfd66af2c28634aaf34ed5429b6
Jeremy Brady <rhspunkerkid05@yahoo.com>
2019-04-02 15:40:15 -0700

    one stretch done

--- assignments/closure.js
+++ assignments/closure.js
@@ -18,11 +18,17 @@ hive();
 // ==== Challenge 2: Create a counter function ====
 const counter = () => {
   // Return a function that when invoked increments and returns a counter variable.
-};
-// Example usage: const newCounter = counter();
-// newCounter(); // 1
-// newCounter(); // 2
+  let count = 0;
+  return function () {
+    return ++count;
+  }
 
+};
+const newCounter = counter();
+console.log(newCounter());
+console.log(newCounter());
+console.log(newCounter());
+console.log(newCounter());
 // ==== Challenge 3: Create a counter function with an object that can increment and decrement ====
 const counterFactory = () => {
   // Return an object that has two methods called `increment` and `decrement`.
