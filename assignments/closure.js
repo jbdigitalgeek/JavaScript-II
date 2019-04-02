bc0e76f8b2a94da631ce12fae5d903abde1f6987
Jeremy Brady <rhspunkerkid05@yahoo.com>
2019-04-02 15:14:22 -0700

    closure done

--- assignments/closure.js
+++ assignments/closure.js
@@ -1,5 +1,15 @@
 // ==== Challenge 1: Write your own closure ====
 // Write a simple closure of your own creation.  Keep it simple!
+function hive() {
+  const queenBee = "Queen Pearl";
+  console.log(`${queenBee} rules!`);
+  function honeyComb() {
+    console.log (`Honeycomb says ${queenBee} lives here!`)
+  }
+  honeyComb();
+}
+hive();
+
 
 
 /* STRETCH PROBLEMS, Do not attempt until you have completed all previous tasks for today's project files */
